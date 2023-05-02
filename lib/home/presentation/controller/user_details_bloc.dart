89ab68876ce914b17bb97955031bb9716db8cc98
Khaatora <ahosari20@gmail.com>
2023-05-02 03:24:43 +0300

    major changes to allow phone signin

--- lib/home/presentation/controller/user_details_bloc.dart
+++ lib/home/presentation/controller/user_details_bloc.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
 import 'dart:developer';
 
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:uber_own/core/usecase/base_usecase.dart';
 import 'package:uber_own/home/domain/usecase/send_phone_verification_code_use_case.dart';
@@ -21,12 +19,14 @@ class UserDetailsBloc extends Bloc<BaseUserDetailsEvent, UserDetailsState> {
   final VerifyUserPhoneUseCase verifyUserPhoneUseCase;
   final SendPhoneVerificationCodeUseCase sendPhoneVerificationCodeUseCase;
 
-  final StreamController<String> _verificationId = StreamController.broadcast();
+  String? _verificationId;
 
-  StreamController<String> get getVerificationIdStreamController =>
-      _verificationId;
+  String? get getVerificationId => _verificationId;
 
-  UserDetailsBloc(this.addUserSharedPrefsDataUseCase, this.verifyUserPhoneUseCase, this.sendPhoneVerificationCodeUseCase)
+  set setVerificationId(String id) => _verificationId = id;
+
+  UserDetailsBloc(this.addUserSharedPrefsDataUseCase,
+      this.verifyUserPhoneUseCase, this.sendPhoneVerificationCodeUseCase)
       : super(const UserDetailsState()) {
     on<VerifyPhoneNumberEvent>(_verifyPhoneNumberEvent);
     on<SendVerificationInfoEvent>(_sendVerificationInfoEvent);
@@ -34,55 +34,82 @@ class UserDetailsBloc extends Bloc<BaseUserDetailsEvent, UserDetailsState> {
     on<ReturnToFirstScreenEvent>(_returnToFirstScreenEvent);
   }
 
-  static UserDetailsBloc get(BuildContext context) => BlocProvider.of(context);
+  static UserDetailsBloc get(context) => BlocProvider.of(context);
 
-  FutureOr<void> _verifyPhoneNumberEvent(VerifyPhoneNumberEvent event,
-      Emitter<UserDetailsState> emit) async {
-    final result = await verifyUserPhoneUseCase(
-        PhoneNumberDetailsParams(event.phoneNum));
-    result.fold((l) =>
-        emit(state.copyWith(
+  FutureOr<void> _verifyPhoneNumberEvent(
+      VerifyPhoneNumberEvent event, Emitter<UserDetailsState> emit) async {
+    final result = await verifyUserPhoneUseCase(PhoneNumberDetailsParams(
+        event.phoneNum,
+        codeSent: _codeSent,
+        codeAutoRetrievalTimeout: _codeAutoRetrievalTimeout));
+    result.fold(
+        (l) => emit(state.copyWith(
             verificationState: VerificationState.error,
             message: l.message)), (r) {
-      log("awaiting user input");
+      log("awaiting verification id");
+
       emit(state.copyWith(
-        phoneNum: event.phoneNum, verificationState: VerificationState.awaitingUserInput,));
-    }
-        );
+        phoneNum: event.phoneNum,
+        verificationState: VerificationState.awaitingVerificationId,
+      ));
+    });
   }
 
-  Future<FutureOr<void>> _sendVerificationInfoEvent(SendVerificationInfoEvent event,
-      Emitter<UserDetailsState> emit) async {
-    final result = await sendPhoneVerificationCodeUseCase(PhoneNumberDetailsParams(event.phoneNum, await getVerificationIdStreamController.stream.last, event.smsCode));
-    result.fold((l) =>
-        emit(state.copyWith(
+  Future<FutureOr<void>> _sendVerificationInfoEvent(
+      SendVerificationInfoEvent event, Emitter<UserDetailsState> emit) async {
+    log(_verificationId ?? "null");
+    final result = await sendPhoneVerificationCodeUseCase(
+        PhoneNumberDetailsParams(event.phoneNum,
+            verificationId: _verificationId!, smsCode: event.smsCode));
+    result.fold(
+        (l) => emit(state.copyWith(
             verificationState: VerificationState.error,
             message: l.message)), (r) {
       log("${event.runtimeType}: ${r.toString()}");
       emit(state.copyWith(
-        phoneNum: r.phoneNumber, verificationState: r.verificationState,));
-    }
-        );
+        phoneNum: r.phoneNumber,
+        verificationState: VerificationState.caching,
+      ));
+    });
   }
 
-
-  FutureOr<void> _cacheDataEvent(CacheDataEvent event, Emitter<UserDetailsState> emit) async {
-    final result = await addUserSharedPrefsDataUseCase(SharedPrefsDataParams(false, true, state.phoneNum!));
-    result.fold((l) =>
-        emit(state.copyWith(
-            verificationState: VerificationState.error,
-            message: l.message)), (r) =>
-        emit(state.copyWith(
-          phoneNum: state.phoneNum, verificationState: VerificationState.completed,)));
+  FutureOr<void> _cacheDataEvent(
+      CacheDataEvent event, Emitter<UserDetailsState> emit) async {
+    final result = await addUserSharedPrefsDataUseCase(
+        SharedPrefsDataParams(false, true, state.phoneNum!));
+    result.fold(
+        (l) => emit(state.copyWith(
+            verificationState: VerificationState.error, message: l.message)),
+        (r) => emit(state.copyWith(
+              phoneNum: state.phoneNum,
+              verificationState: VerificationState.completed,
+            )));
   }
 
-  FutureOr<void> _returnToFirstScreenEvent(ReturnToFirstScreenEvent event, Emitter<UserDetailsState> emit) {
-    emit(state.copyWith(message: "", verificationState: VerificationState.init, phoneNum: "", smsCode: "",));
+  FutureOr<void> _returnToFirstScreenEvent(
+      ReturnToFirstScreenEvent event, Emitter<UserDetailsState> emit) {
+    emit(state.copyWith(
+      message: "",
+      verificationState: VerificationState.init,
+      phoneNum: "",
+      smsCode: "",
+    ));
   }
 
-  void dispose(){
-    getVerificationIdStreamController.close();
+  void dispose() {}
+
+  void _codeSent(String verificationId, int? forceResendingToken) {
+    //code is sent
+    setVerificationId = verificationId;
+    log("myVerificationId: $verificationId, verification ID: $verificationId");
+    emit(
+        state.copyWith(verificationState: VerificationState.awaitingUserInput));
   }
 
-
+  void _codeAutoRetrievalTimeout(String verificationId) {
+    setVerificationId = verificationId;
+    log("myVerificationId: $verificationId, verification ID: $verificationId");
+    emit(
+        state.copyWith(verificationState: VerificationState.awaitingUserInput));
+  }
 }
