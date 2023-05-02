8bae266b14b333c5f1a4232e83b11663e7e2fca9
Khaatora <ahosari20@gmail.com>
2023-05-02 03:24:09 +0300

    added email field and serializable credential

--- lib/home/domain/entity/user.dart
+++ lib/home/domain/entity/user.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:uber_own/core/utils/enums/sign_in_method.dart';
 import 'package:uber_own/core/utils/enums/verification_enums.dart';
 
 class MyUser extends Equatable {
@@ -6,15 +7,21 @@ class MyUser extends Equatable {
   final VerificationState verificationState;
   final String? verificationCode;
   final String? email;
+  final SignInMethod method;
 
-
-
-  const MyUser(this.phoneNumber, this.verificationState, [this.verificationCode = '', this.email = '']);
+  const MyUser([
+    this.phoneNumber = "unknown",
+    this.verificationState = VerificationState.init,
+    this.verificationCode = '',
+    this.email = 'unknown',
+    this.method = SignInMethod.phoneNumber,
+  ]);
 
   @override
   List<Object?> get props => [
         phoneNumber,
         verificationState,
-
+        email,
+        method,
       ];
 }
